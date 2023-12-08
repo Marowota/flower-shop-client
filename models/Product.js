e55d136230b8643f3b9c629f1fb376279cb7ae6f
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-12-09 02:31:05 +0700

    add new product things

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,15 @@
 const { model, Schema, models, default: mongoose } = require("mongoose");
 
-const ProdcutSchema = new Schema({
-  title: { type: String, required: true },
-  description: String,
-  price: { type: Number, required: true },
-  images: [{ type: String }],
-  category: { type: mongoose.Types.ObjectId, ref: "Category" },
-  properties: { type: Object },
-});
+const ProdcutSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    properties: { type: Object },
+  },
+  { timestamps: true }
+);
 
 export const Product = models.Product || model("Product", ProdcutSchema);
