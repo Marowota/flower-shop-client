df2d5cb556cdbd6b8a251c193c18836d8e808a20
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-12-11 21:40:48 +0700

    Add nav bar effect

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 import Center from "./Center";
 import { useContext } from "react";
@@ -31,7 +32,11 @@ const NavLink = styled(Link)`
 `;
 
 export default function Header() {
+  const inactiveLink = " text-rose-400 hover:text-rose-500";
+  const activeLink = " text-rose-600 font-semibold hover:text-rose-700";
   const { cartProducts } = useContext(CartContext);
+  const router = useRouter();
+  const { pathname } = router;
   return (
     <div className="bg-gradient-to-r from-cyan-200 via-rose-200 to-pink-200">
       <Center>
@@ -42,12 +47,43 @@ export default function Header() {
           >
             Flower Shop
           </Link>
-          <div className="flex gap-5 text-rose-500 items-center">
-            <Link href={"/"}>Home</Link>
-            <Link href={"/products"}>All flowers</Link>
-            <Link href={"/categories"}>Categories</Link>
-            <Link href={"/account"}>Account</Link>
-            <Link href={"/cart"}>Cart ({cartProducts.length})</Link>
+          <div className="flex gap-5 items-center">
+            <Link
+              href={"/"}
+              className={pathname === "/" ? activeLink : inactiveLink}
+            >
+              Home
+            </Link>
+            <Link
+              href={"/products"}
+              className={
+                pathname.includes("/products") ? activeLink : inactiveLink
+              }
+            >
+              All flowers
+            </Link>
+            <Link
+              href={"/categories"}
+              className={
+                pathname.includes("/categories") ? activeLink : inactiveLink
+              }
+            >
+              Categories
+            </Link>
+            <Link
+              href={"/account"}
+              className={
+                pathname.includes("/account") ? activeLink : inactiveLink
+              }
+            >
+              Account
+            </Link>
+            <Link
+              href={"/cart"}
+              className={pathname.includes("/cart") ? activeLink : inactiveLink}
+            >
+              Cart ({cartProducts.length})
+            </Link>
           </div>
         </Wrapper>
       </Center>
